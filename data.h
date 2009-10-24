cc6735cb65c24c5a3e201793279f5a2b61c9c263
manuel.angel.abeledo.garcia <manuel.angel.abeledo.garcia@dc96859c-684f-0410-990c-99a9d91b151d>
2009-10-24 21:29:06 +0000

    GPLv2 documentation added to each file.

--- data.h
+++ data.h
@@ -1,12 +1,30 @@
 /********************************************************************
- *  Proyecto: Sistema modular de comunicacion con un robot movil
- *  Subproyecto: Servidor
- *  Archivo: data.h
- * 	Version: 0.1
- * 
+ *  Project: modcom-server
+ *  File: data.h
+ *  Version: 1.0b
+ *  
  *  Defines data structures.
+ * 
+ *  Author: Manuel Angel Abeledo Garcia
  *
- *  Autor: Manuel Angel Abeledo Garcia
+ ************************ License ***********************************
+ * 
+ *  This file is part of modcom-server.
+ *
+ *  modcom-server is free software: you can redistribute it and/or 
+ *  modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation, either version 2 of
+ *  the License, or (at your option) any later version.
+ *
+ *  modcom-server is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with modcom-server.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
  ********************************************************************/
 
 //#include "header.h"
--- plugin.h
+++ plugin.h
@@ -1,10 +1,28 @@
 /********************************************************************
- *  Proyecto: Sistema modular de comunicacion con un robot movil
- *  Subproyecto: Servidor
- *  Archivo: plugin.h
- *  Version: 1.0
+ *  Project: modcom-server
+ *  File: plugin.h
+ *  Version: 1.0b
+ * 
+ *  Author: Manuel Angel Abeledo Garcia
  *
- *  Autor: Manuel Angel Abeledo Garcia
+ ************************ License ***********************************
+ * 
+ *  This file is part of modcom-server.
+ *
+ *  modcom-server is free software: you can redistribute it and/or 
+ *  modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation, either version 2 of
+ *  the License, or (at your option) any later version.
+ *
+ *  modcom-server is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with modcom-server.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
  ********************************************************************/
 
 #include <gmodule.h>
